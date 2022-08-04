c87caed343510daa20f81c05a1ff6da38dfeb5d2
Cloakd <thecloakdone@gmail.com>
2022-08-04 10:24:16 +0100

    Transfer view mockup

--- src/views/TransferView.js
+++ src/views/TransferView.js
@@ -7,9 +7,24 @@ export default class TransferView extends AbstractView {
   }
 
   async getHtml() {
-    return `
-            <h1>Transfer</h1>
-            <p>Powered By Jupiter AG</p>
-        `;
+    return `<div class="swap text-center">
+	<h1>Transfer</h1>
+	<p>Powered By Jupiter AG</p>
+
+	<div class="row mt-3">
+		<div class="col-12">
+			<input class="form-control" placeholder="Input Token">
+		</div>
+		<div class="col-12 mt-3"></div>
+
+		<div class="col-12 mt-3">
+			<input class="form-control" placeholder="Output Token">
+		</div>
+
+		<div class="col-12 text-center mt-3">
+		    <button class="btn btn-primary btn-block">SWAP</button>
+		</div>
+	</div>
+</div>`;
   }
 }
